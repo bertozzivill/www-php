847312ecc9607b8aefd2c3a29709684de251ba3e
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2026-03-14 11:20:50 -0700

    remove commented line for now

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -23,7 +23,7 @@
                 <li><a href="https://idmod.org/" target="_blank">Work</a></li>
                 <li><a href="/publications">Publications</a></li>
                 <li><a href="https://medium.com/@abertozz" target="_blank">Blog</a></li>
-                // <li><a href="https://twitter.com/abertozz" target="_blank">@abertozz</a></li>
+                <li><a href="https://twitter.com/abertozz" target="_blank">@abertozz</a></li>
             </ul>
         </nav>
     </header>
